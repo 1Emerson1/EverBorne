ab334b52823cc9b15b21ddff2d5e6faa45a5f397
Emerson H <hidalgoemerson@gmail.com>
2018-03-25 15:40:00 -0400

    Improved health script and added knock back mechanic

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -3,43 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthScript : MonoBehaviour {
-
-    public GameObject Parent;
     private const int maxHealth = 100;
-    public const int decreaseHealth = 5;
     public int currentHealth = maxHealth;
+	public const int decreaseHealth = 5;
 
 	// displays current health upon start
-	void start() {
+	void Start() {
 		Debug.Log("Current health - " + currentHealth);
 	}
 
-    // when player(sphere) collides with wall, deduct 5 points
-    void OnCollisionEnter(Collision collision) {
-
-		// setting knock back variables
-		var knockBack = 5000;
-		var force = transform.position - collision.transform.position;
-		force.Normalize();
-
-		// if health hits 0, quit game
-		if (currentHealth != 0) {
-			if(collision.gameObject.tag == "wall") {
-				Debug.Log("Whoops, you hit a wall!");
-				// knocks player back
-				Debug.Log("Pushing you back!");
-				//gameObject.Get
-
-				// decreases health
-				Debug.Log("Player Health has decreased by " + decreaseHealth + " points");
-				currentHealth -= decreaseHealth;
-
-				// displays current health
-				Debug.Log("Current Health : " + currentHealth);
-			}
-		} else {
-			Debug.Log("You're dead, ripperoni");
-			Application.Quit();
+	// method to apply damage to player's health
+	public void TakeDamage() {
+		currentHealth -= decreaseHealth;
+		Debug.Log("Health - " + currentHealth);
+		if(currentHealth <= 0) {
+			currentHealth = 0;
+			Debug.Log("Dead!");
+			
 		}
-    }
+	}
+
+    // when player collides with wall, knock player back 
+	// and deduct 5 health points
+    void OnCollisionEnter(Collision collision) {
+		// setting knock back variables	
+		var knockBack = 5500;
+
+		if(collision.gameObject.tag == "wall") {
+			// apply damage to player
+			TakeDamage();
+
+			Debug.Log("Knocking you back");
+			// knock player back
+			gameObject.GetComponent<Rigidbody>().AddForce(-transform.forward * knockBack);
+		}
+	}
 }
