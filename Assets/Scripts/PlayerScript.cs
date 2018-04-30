77fbf0140c25e4f1ac2f7e82a71d4b78d4154299
1emerson1 <hidalgoemerson@gmail.com>
2018-04-30 13:34:19 -0400

    Added Health UI and music

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,33 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthScript : MonoBehaviour {
-    private const int maxHealth = 100;
+public class PlayerScript : MonoBehaviour {
+	// timer variables
+	public Text counterText;
+	public float seconds, minutes;
+	public float timeLeft;
+	// health variables
+    //private const int maxHealth = 100;
     public int currentHealth;
 	public const int decreaseHealth = 5;
+	// ui variables
 	public TextMesh gameOverText;
 	public Text healthText;
 
 	// setup function
 	void Start() {
-		// set initial health
-		currentHealth = maxHealth;
-		
-		healthText = GetComponent<Text>() as Text;
+		counterText = GameObject.Find("counterText").GetComponent<Text>();
+		healthText = GameObject.Find("healthText").GetComponent<Text>();
 
-		//healthText.text = "Health: " + currentHealth.ToString();
-		Debug.Log("Current health - " + currentHealth);
+		healthText.text = "Health: " + currentHealth.ToString();
+		//Debug.Log("Current health - " + currentHealth);
+
+		// initializing timer
+		StartCoroutine("LoseTime");
+		Time.timeScale = 1;
 
 		// finding TextMesh object
 		gameOverText = GameObject.Find("endGameText").GetComponent<TextMesh>();
 		gameOverText.text = "Game over!";
 	}
 
+	// Update is called once per frame
+	void Update () {
+		minutes = (int)(timeLeft/60f);
+		seconds = (int)(timeLeft % 60f);
+
+		counterText.text = "Time Left: " + minutes.ToString() + ":" + seconds.ToString("00");
+
+		if(timeLeft == 0) {
+			gameOverText.text = "Time is up!";
+			endGame();
+		}
+	}
+
 	// method to apply damage to player's health
 	public void TakeDamage() {
 		currentHealth -= decreaseHealth;
 
-		//healthText.text = "Health: " + currentHealth.ToString();
+		healthText.text = "Health: " + currentHealth.ToString();
 
 		Debug.Log("Health - " + currentHealth);
 		if(currentHealth == 0) {
@@ -39,7 +60,9 @@ public class HealthScript : MonoBehaviour {
 		}
 	}
 
+	// teleports player to end game area
 	public void endGame() {
+		counterText.enabled = false;
 		this.gameObject.transform.position = new Vector3(-12.6f, 2.7f, -31.26f);
 	}
 
@@ -65,4 +88,12 @@ public class HealthScript : MonoBehaviour {
 			endGame();
         }
     }
+
+	// timer method
+	IEnumerator LoseTime() {
+		while (timeLeft > 0) {
+			yield return new WaitForSeconds(1);
+			timeLeft--;
+		}
+	}
 }
